8571845892610021735180aa588b6cc6f6f91bff
hailmarvin <mmwanzi76@gmail.com>
2021-03-05 19:22:05 +0300

    databse table changes

--- priv/repo/migrations/20210224172416_create_patients.exs
+++ priv/repo/migrations/20210224172416_create_patients.exs
@@ -5,8 +5,12 @@ defmodule Hosp.Repo.Migrations.CreatePatients do
     create table(:patients) do
       add :firstname, :string
       add :surname, :string
+      add :email, :string
+      add :phone, :integer
       add :age, :integer
       add :gender, :string
+      add :registration, :integer
+      add :hash_password, :string
 
       timestamps()
     end
--- priv/repo/migrations/20210304160512_create_staffs.exs
+++ priv/repo/migrations/20210304160512_create_staffs.exs
@@ -4,10 +4,14 @@ defmodule Hosp.Repo.Migrations.CreateStaffs do
   def change do
     create table(:staffs) do
       add :name, :string
+      add :surname, :string
+      add :email, :string
+      add :phone, :integer
       add :department, :string
       add :registration, :integer
       add :gender, :string
       add :position, :string
+      add :hash_password, :string
 
       timestamps()
     end

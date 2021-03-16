9b9ffb02aeedc2a3b08e06017caa53b54074b93b
hailmarvin <mmwanzi76@gmail.com>
2021-03-16 22:11:00 +0300

    dynamic header

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -11,6 +11,17 @@ export class AppComponent {
   panelOpenState = false;
 
   constructor(private route: Router) {}
+
+  @HostListener('window:scroll', ['$event'])
+
+  onWindowScroll(e) {
+    let element = document.querySelector('.header');
+    if (window.pageYOffset > 700) {
+      element.classList.add('header-changed');
+    } else {
+      element.classList.remove('header-changed');
+    }
+  }
 
   account() {
     this.route.navigateByUrl('account/signin')

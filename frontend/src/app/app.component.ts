242417649ac868a055919c1403acc33756002060
hailmarvin <mmwanzi76@gmail.com>
2021-05-23 22:05:50 +0300

    Landing page

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+  @HostListener('window:scroll', ['$event'])
+
+  onWindowScroll(e) {
+    let element = document.querySelector('.header');
+    if (window.pageYOffset > 700) {
+      element.classList.add('header-dark');
+    } else {
+      element.classList.remove('header-dark');
+    }
+  }
 }

2d8a7aacb77ad628d7b77ad5888181c059a44b3d
hailmarvin <mmwanzi76@gmail.com>
2021-05-25 14:05:32 +0300

    updated ui v1.1

--- frontend/src/app/account/account.module.ts
+++ frontend/src/app/account/account.module.ts
@@ -1,22 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SigninComponent } from './signin/signin.component';
 import { LoginComponent } from './login/login.component';
-import { ProfileComponent } from './profile/profile.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from '../material.module';
 
 @NgModule({
-  declarations: [SigninComponent, LoginComponent, ProfileComponent],
+  declarations: [LoginComponent],
   imports: [
     CommonModule,
+    MaterialModule,
     ReactiveFormsModule,
     RouterModule.forChild([
-        { path: 'signin', component: SigninComponent },
+        // { path: 'signin', component: SigninComponent },
         { path: 'login', component: LoginComponent }
-    ]),
-    MaterialModule
+    ])
   ]
 })
 export class AccountModule { }
